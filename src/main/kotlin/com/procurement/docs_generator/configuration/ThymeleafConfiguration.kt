ab6e3ab087aed7c66940864198952a72e3457059
Maxim Sambulat <maxim.sambulat@gmail.com>
2018-11-27 13:59:49 +0200

    Added TimeDialect to Thymeleaf

--- src/main/kotlin/com/procurement/docs_generator/configuration/ThymeleafConfiguration.kt
+++ src/main/kotlin/com/procurement/docs_generator/configuration/ThymeleafConfiguration.kt
@@ -3,6 +3,7 @@ package com.procurement.docs_generator.configuration
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.thymeleaf.TemplateEngine
+import org.thymeleaf.extras.java8time.dialect.Java8TimeDialect
 import org.thymeleaf.spring5.SpringTemplateEngine
 import org.thymeleaf.templateresolver.ITemplateResolver
 import org.thymeleaf.templateresolver.StringTemplateResolver
@@ -12,14 +13,14 @@ class ThymeleafConfiguration {
 
     @Bean
     fun templateEngine(): TemplateEngine {
-        val templateEngine = SpringTemplateEngine()
-        templateEngine.addTemplateResolver(stringTemplateResolver())
-        return templateEngine
+        val engine = SpringTemplateEngine()
+        engine.addTemplateResolver(stringTemplateResolver())
+        engine.addDialect(Java8TimeDialect())
+        return engine
     }
 
     private fun stringTemplateResolver(): ITemplateResolver {
         val templateResolver = StringTemplateResolver()
-        templateResolver.order = Integer.valueOf(3)
         templateResolver.setTemplateMode("HTML")
         templateResolver.isCacheable = false
         return templateResolver
