f15dd68fc833f87e9f9fe92c129b59372e273f2f
Maxim Sambulat <maxim.sambulat@gmail.com>
2018-11-28 09:50:12 +0200

    Refactoring

--- src/main/kotlin/com/procurement/docs_generator/exception/app/InvalidValueOfParamException.kt
+++ src/main/kotlin/com/procurement/docs_generator/exception/app/InvalidValueOfParamException.kt
@@ -6,5 +6,5 @@ import com.procurement.docs_generator.infrastructure.dispatcher.CodesOfErrors
 class InvalidValueOfParamException(nameAndTypeParam: String, valueParam: String) : ApplicationException(
     loglevel = Logger.Level.ERROR,
     codeError = CodesOfErrors.INVALID_VALUE_OF_PARAM,
-    message = "Invalid value: '$valueParam' of '$nameAndTypeParam'."
+    message = "Invalid value: '$valueParam' of $nameAndTypeParam."
 )
