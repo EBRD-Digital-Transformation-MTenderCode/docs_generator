c5d1a0080f607ff5c8b07e4d0f4ad061a1118410
Maxim Sambulat <maxim.sambulat@gmail.com>
2018-12-26 16:13:49 +0200

    Changed context of Services [commit:5900e37c]

--- src/main/kotlin/com/procurement/docs_generator/domain/model/document/context/mapper/ServicesContextMapper.kt
+++ src/main/kotlin/com/procurement/docs_generator/domain/model/document/context/mapper/ServicesContextMapper.kt
@@ -29,7 +29,7 @@ object ServicesContextMapper {
                 },
                 tender = acRelease.tender.let { tender ->
                     ServicesContext.AC.Tender(
-                        procurementMethodDetails = tender.procurementMethodDetails,
+                        procurementMethodDetails = getProcurementMethodDetails(msRelease), //tender.procurementMethodDetails,
                         classification = tender.classification.let { classification ->
                             ServicesContext.AC.Tender.Classification(
                                 id = classification.id,
@@ -66,9 +66,14 @@ object ServicesContextMapper {
                             ServicesContext.AC.Buyer.Person(
                                 title = person.title,
                                 name = person.name,
-                                businessFunctions = person.businessFunctions.mapBusinessFunctionByType(type = "authority") {
+                                businessFunctions = person.businessFunctions.mapBusinessFunctionByType(type = "authority") { businessFunction ->
                                     ServicesContext.AC.Buyer.Person.BusinessFunction(
-                                        jobTitle = it.jobTitle
+                                        jobTitle = businessFunction.jobTitle,
+                                        documents = businessFunction.documents.mapDocumentsByDocumentType(documentType = "regulatoryDocument") { document ->
+                                            ServicesContext.AC.Buyer.Person.BusinessFunction.Document(
+                                                title = document.title
+                                            )
+                                        }
                                     )
                                 }
                             )
@@ -95,7 +100,7 @@ object ServicesContextMapper {
                                 legalForm = ServicesContext.AC.Buyer.Details.LegalForm(
                                     detail.legalForm.description
                                 ),
-                                permit = detail.permits.firstOrNullPermitByScheme(scheme = "MD-SRLE") { permit ->
+                                permit = detail.permits.firstOrNullPermitByScheme(scheme = "SRLE") { permit ->
                                     ServicesContext.AC.Buyer.Details.Permit(
                                         id = permit.id,
                                         startDate = permit.permitDetails.validityPeriod.startDate.toLocalDate()
@@ -167,7 +172,7 @@ object ServicesContextMapper {
                                 legalForm = ServicesContext.AC.Supplier.Details.LegalForm(
                                     description = detail.legalForm.description
                                 ),
-                                permit = detail.permits.firstOrNullPermitByScheme(scheme = "MD-SRLE") { permit ->
+                                permit = detail.permits.firstOrNullPermitByScheme(scheme = "SRLE") { permit ->
                                     ServicesContext.AC.Supplier.Details.Permit(
                                         id = permit.id,
                                         startDate = permit.permitDetails.validityPeriod.startDate.toLocalDate()
@@ -351,6 +356,37 @@ object ServicesContextMapper {
         }
 
         throw IllegalStateException("Contract not contains '$ccSubject'")
+    }
+
+    private fun getProcurementMethodDetails(msRelease: MSReleasesPackage.Release): String {
+        val mainProcurementCategory = msRelease.tender.mainProcurementCategory.toUpperCase()
+        return when (mainProcurementCategory) {
+            "GOODS" -> {
+                val amount = msRelease.tender.value.amount.toLong()
+                when {
+                    amount < 80000 -> "mv"
+                    amount in 80000..400000 -> "sv"
+                    else -> "ot"
+                }
+            }
+            "SERVICES" -> {
+                val amount = msRelease.tender.value.amount.toLong()
+                when {
+                    amount < 80000 -> "mv"
+                    amount in 80000..400000 -> "sv"
+                    else -> "ot"
+                }
+            }
+            "WORKS" -> {
+                val amount = msRelease.tender.value.amount.toLong()
+                when {
+                    amount < 100000 -> "mv"
+                    amount in 100000..1500000 -> "sv"
+                    else -> "ot"
+                }
+            }
+            else -> throw IllegalStateException("Unknown mainProcurementCategory: '$mainProcurementCategory'.")
+        }
     }
 
     private class ContractAgreedMetrics(props: Map<String, String>) {
--- src/main/kotlin/com/procurement/docs_generator/domain/model/release/MSReleasesPackage.kt
+++ src/main/kotlin/com/procurement/docs_generator/domain/model/release/MSReleasesPackage.kt
@@ -3,6 +3,7 @@ package com.procurement.docs_generator.domain.model.release
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties
 import com.fasterxml.jackson.annotation.JsonProperty
 import com.fasterxml.jackson.annotation.JsonPropertyOrder
+import java.math.BigDecimal
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 @JsonPropertyOrder("releases")
@@ -13,10 +14,17 @@ class MSReleasesPackage(
     data class Release(
         @field:JsonProperty("tender") @param:JsonProperty("tender") val tender: Tender
     ) {
-        @JsonPropertyOrder("id", "title")
+        @JsonPropertyOrder("id", "title", "mainProcurementCategory", "value")
         data class Tender(
             @field:JsonProperty("id") @param:JsonProperty("id") val id: String,
-            @field:JsonProperty("title") @param:JsonProperty("title") val title: String
-        )
+            @field:JsonProperty("title") @param:JsonProperty("title") val title: String,
+            @field:JsonProperty("mainProcurementCategory") @param:JsonProperty("mainProcurementCategory") val mainProcurementCategory: String,
+            @field:JsonProperty("value") @param:JsonProperty("value") val value: Value
+        ) {
+            @JsonPropertyOrder("amount")
+            data class Value(
+                @field:JsonProperty("amount") @param:JsonProperty("amount") val amount: BigDecimal
+            )
+        }
     }
 }
