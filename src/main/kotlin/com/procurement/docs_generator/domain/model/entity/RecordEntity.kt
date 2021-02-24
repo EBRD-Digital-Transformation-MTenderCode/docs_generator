fd201930a0f53ac39c6e1ddf1475c778bf11dc36
katherine <kate.shaidurova@gmail.com>
2021-02-24 23:04:09 +0200

    ES-5623 Renamed RecordReleaseType to RecordName

--- src/main/kotlin/com/procurement/docs_generator/domain/model/entity/RecordEntity.kt
+++ src/main/kotlin/com/procurement/docs_generator/domain/model/entity/RecordEntity.kt
@@ -3,7 +3,7 @@ package com.procurement.docs_generator.domain.model.entity
 import com.procurement.docs_generator.domain.model.country.Country
 import com.procurement.docs_generator.domain.model.language.Language
 import com.procurement.docs_generator.domain.model.pmd.ProcurementMethod
-import com.procurement.docs_generator.domain.model.pmd.RecordReleaseType
+import com.procurement.docs_generator.domain.model.pmd.RecordName
 import com.procurement.docs_generator.domain.model.pmd.RelatedProcessType
 
 class RecordEntity(
@@ -11,7 +11,7 @@ class RecordEntity(
     val country: Country,
     val lang: Language,
     val documentInitiator: String,
-    val mainProcess: RecordReleaseType,
+    val mainProcess: RecordName,
     val relationships: Relationships
 ) {
     class Relationships(values: List<RelatedProcessType>) : List<RelatedProcessType> by values
--- src/main/kotlin/com/procurement/docs_generator/domain/model/pmd/RecordName.kt
+++ src/main/kotlin/com/procurement/docs_generator/domain/model/pmd/RecordName.kt
@@ -3,7 +3,7 @@ package com.procurement.docs_generator.domain.model.pmd
 import com.fasterxml.jackson.annotation.JsonCreator
 import com.procurement.docs_generator.domain.model.EnumElementProvider
 
-enum class RecordReleaseType(override val key: String) : EnumElementProvider.Key {
+enum class RecordName(override val key: String) : EnumElementProvider.Key {
     AC("AC"),
     EV("EV"),
     FE("FE"),
@@ -11,10 +11,10 @@ enum class RecordReleaseType(override val key: String) : EnumElementProvider.Key
 
     override fun toString(): String = key
 
-    companion object : EnumElementProvider<RecordReleaseType>(info = info()) {
+    companion object : EnumElementProvider<RecordName>(info = info()) {
 
         @JvmStatic
         @JsonCreator
-        fun creator(name: String) = RecordReleaseType.orThrow(name)
+        fun creator(name: String) = RecordName.orThrow(name)
     }
 }
