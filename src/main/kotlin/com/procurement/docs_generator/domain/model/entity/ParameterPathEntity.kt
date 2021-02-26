4aa5906ea08c05f2004075327edf5f8adf4d8566
katherine <kate.shaidurova@gmail.com>
2021-02-26 09:46:36 +0200

    ES-5623 Removed redundant Relationships, added data keyword

--- src/main/kotlin/com/procurement/docs_generator/domain/model/entity/ParameterPathEntity.kt
+++ src/main/kotlin/com/procurement/docs_generator/domain/model/entity/ParameterPathEntity.kt
@@ -5,17 +5,14 @@ import com.fasterxml.jackson.annotation.JsonValue
 import com.procurement.docs_generator.domain.model.EnumElementProvider
 import com.procurement.docs_generator.domain.model.pmd.ProcurementMethod
 import com.procurement.docs_generator.domain.model.pmd.RecordName
-import com.procurement.docs_generator.domain.model.pmd.RelatedProcessType
 
-class ParameterPathEntity(
+data class ParameterPathEntity(
     val pmd: ProcurementMethod,
     val documentInitiator: String,
     val parameter: Parameter,
     val record: RecordName,
     val path: String
 ) {
-    class Relationships(values: List<RelatedProcessType>) : List<RelatedProcessType> by values
-
     enum class Parameter(@JsonValue override val key: String) : EnumElementProvider.Key {
 
         DATE("date"),
