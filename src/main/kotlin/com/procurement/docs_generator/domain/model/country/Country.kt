4ddf022e0c20811fa7239a3c0939cb949db0d982
katherine <kate.shaidurova@gmail.com>
2021-03-10 18:28:13 +0200

    ES-5746 Added toString override

--- src/main/kotlin/com/procurement/docs_generator/domain/model/country/Country.kt
+++ src/main/kotlin/com/procurement/docs_generator/domain/model/country/Country.kt
@@ -2,4 +2,6 @@ package com.procurement.docs_generator.domain.model.country
 
 import com.procurement.docs_generator.domain.model.ValueObject
 
-data class Country(val value: String) : ValueObject
+data class Country(val value: String) : ValueObject {
+    override fun toString() = value
+}
